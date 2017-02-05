d2b588120616ae7e3a837a2a16c5971e9b00d391
Hai Joey Tran <htran@tgen.org>
2017-02-05 01:02:01 -0500

    glued first page

--- pages.js
+++ pages.js
@@ -4,6 +4,22 @@ import { SpringGrid, CSSGrid, measureItems, makeResponsive  } from 'react-stonec
 import {ScrollBox, ScrollAxes, FastTrack} from 'react-scroll-box';
 
 
+///will call the callback when it returns. 
+///The object the callback passes will be a json list of representative objects
+///pass in the zipcode string to get the representitives for a specified zip code
+function httpGetReps(zipcode,callback)
+{
+	var url = 'https://hab2017.azurewebsites.net/api/reps/'+zipcode;
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+
+    xmlHttp.open("GET", url, true); // true for asynchronous 
+	xmlHttp.setRequestHeader( 'Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8' );
+    xmlHttp.send();
+}
 
 
 var FirstPage = React.createClass({
@@ -30,20 +46,26 @@ var FirstPage = React.createClass({
 
     handleGo: function(e) {
 
-      var issues = ["immigration", "religious freedom"]
-      var reps = [{name : "McCain", state : "Arizona", party : "Republican", picture : "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/John_McCain_official_portrait_2009.jpg/220px-John_McCain_official_portrait_2009.jpg"},
-                  {name : "McCain", state : "Arizona", party : "Republican", picture : "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/John_McCain_official_portrait_2009.jpg/220px-John_McCain_official_portrait_2009.jpg"},
-                  {name : "McCain", state : "Arizona", party : "Republican", picture : "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/John_McCain_official_portrait_2009.jpg/220px-John_McCain_official_portrait_2009.jpg"},
-                  {name : "McCain", state : "Arizona", party : "Republican", picture : "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/John_McCain_official_portrait_2009.jpg/220px-John_McCain_official_portrait_2009.jpg"},
-                  {name : "McCain", state : "Arizona", party : "Republican", picture : "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/John_McCain_official_portrait_2009.jpg/220px-John_McCain_official_portrait_2009.jpg"},
-                  {name : "McCain", state : "Arizona", party : "Republican", picture : "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/John_McCain_official_portrait_2009.jpg/220px-John_McCain_official_portrait_2009.jpg"},
-                  {name : "McCain", state : "Arizona", party : "Republican", picture : "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/John_McCain_official_portrait_2009.jpg/220px-John_McCain_official_portrait_2009.jpg"},
-                  {name : "McCain", state : "Arizona", party : "Republican", picture : "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/John_McCain_official_portrait_2009.jpg/220px-John_McCain_official_portrait_2009.jpg"}]
-      ReactDOM.render(
-        <SecondPage items={ { one : issues, two : reps } } />,
-        document.getElementById('container')
-      );
+        httpGetReps(this.state.searchString, function(resp) {
+              var issues = ["immigration", "religious freedom"]
+              var reps = [{name : "McCain", state : "Arizona", party : "Republican", picture : "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/John_McCain_official_portrait_2009.jpg/220px-John_McCain_official_portrait_2009.jpg"},
+                          {name : "McCain", state : "Arizona", party : "Republican", picture : "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/John_McCain_official_portrait_2009.jpg/220px-John_McCain_official_portrait_2009.jpg"},
+                          {name : "McCain", state : "Arizona", party : "Republican", picture : "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/John_McCain_official_portrait_2009.jpg/220px-John_McCain_official_portrait_2009.jpg"},
+                          {name : "McCain", state : "Arizona", party : "Republican", picture : "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/John_McCain_official_portrait_2009.jpg/220px-John_McCain_official_portrait_2009.jpg"},
+                          {name : "McCain", state : "Arizona", party : "Republican", picture : "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/John_McCain_official_portrait_2009.jpg/220px-John_McCain_official_portrait_2009.jpg"},
+                          {name : "McCain", state : "Arizona", party : "Republican", picture : "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/John_McCain_official_portrait_2009.jpg/220px-John_McCain_official_portrait_2009.jpg"},
+                          {name : "McCain", state : "Arizona", party : "Republican", picture : "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/John_McCain_official_portrait_2009.jpg/220px-John_McCain_official_portrait_2009.jpg"},
+                          {name : "McCain", state : "Arizona", party : "Republican", picture : "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/John_McCain_official_portrait_2009.jpg/220px-John_McCain_official_portrait_2009.jpg"}]
+            var reps = JSON.parse(resp);
+              ReactDOM.render(
+                <SecondPage items={ { one : issues, two : reps } } />,
+                document.getElementById('container'));
+        })
     }
+
+
+        
+      
 
   });
 
@@ -80,10 +102,10 @@ var FirstPage = React.createClass({
 
             <Column width="2/3" className="repPanel">
                 {reps.map(function(r) {return <div className='rep' onClick={function(e) {this.repSelection=r.name; console.log(this.repSelection)}.bind(this)}>
-                <p>{r.name}</p>
+                <p>{r.firstName} {r.lastName}</p>
                 <p>{r.state}</p>
                 <p>{r.party}</p>
-                <img src={r.picture} className="portrait"/>
+                <img src={r.image_url} className="portrait"/>
                 </div>}, this)}
             </Column>
           </Row>
